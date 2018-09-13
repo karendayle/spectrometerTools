0dc84b5a0c9e934ec06196bf15ea349b5be107f6
kadeyo <karendayle@gmail.com>
2018-09-13 14:28:26 -0500

    Version for "pH{4,7,10} dried 2" spectra
    Plot 3 curves gathered from 9/12/18 dried samples with YHY stable (blue) and pH-sensitive (green) peaks.

--- plotAssortedFiles.m
+++ plotAssortedFiles.m
@@ -1,7 +1,7 @@
 % Plot files in different directories
 
 % Dayle Kotturi June 2018
-
+maxIntensity = 0;
 % Read in a set of spectra from a time-series 
 % Read in the name of the FOLDER.
 figure % without this, no plots are drawn
@@ -17,48 +17,63 @@ for K = 1 : 3
           thisdata1 = load(thisfilename); %load just this file
           fprintf( 'File #%d, "%s", maximum value was: %g\n', K, ...
               thisfilename, max(thisdata1(:)) );
+          if (max(thisdata1(:)) > maxIntensity)
+              maxIntensity = max(thisdata1(:));
+          end
       end
-  else if (K == 2) 
-      dir_to_search = 'H:\Documents\Data\pH7 dried 2';
-      txtpattern = fullfile(dir_to_search, 'spectrum*.txt');
-      dinfo = dir(txtpattern);
-      thisfilename = fullfile(dir_to_search, dinfo.name); % just the name
-      thisdata2 = load(thisfilename); %load just this file
-      fprintf( 'File #%d, "%s", maximum value was: %g\n', K, ...
-          thisfilename, max(thisdata2(:)) );
-  else if (K == 3) 
-      dir_to_search = 'H:\Documents\Data\pH10 dried 2';
-      txtpattern = fullfile(dir_to_search, 'spectrum*.txt');
-      dinfo= dir(txtpattern);
-      thisfilename = fullfile(dir_to_search, dinfo.name); % just the name
-      thisdata3 = load(thisfilename); %load just this file
-      fprintf( 'File #%d, "%s", maximum value was: %g\n', K, ...
-          thisfilename, max(thisdata3(:)) );
+  else
+      if (K == 2)
+          dir_to_search = 'H:\Documents\Data\pH7 dried 2';
+          txtpattern = fullfile(dir_to_search, 'spectrum*.txt');
+          dinfo = dir(txtpattern);
+          for (I = 1 : length(dinfo))
+              thisfilename = fullfile(dir_to_search, dinfo(I).name); % just the name
+              thisdata2 = load(thisfilename); %load just this file
+              fprintf( 'File #%d, "%s", maximum value was: %g\n', K, ...
+                  thisfilename, max(thisdata2(:)) );
+              if (max(thisdata2(:)) > maxIntensity)
+                  maxIntensity = max(thisdata2(:));
+              end
+          end
+      else
+          if (K == 3)
+              dir_to_search = 'H:\Documents\Data\pH10 dried 2';
+              txtpattern = fullfile(dir_to_search, 'spectrum*.txt');
+              dinfo= dir(txtpattern);
+              for (I = 1 : length(dinfo))
+                  thisfilename = fullfile(dir_to_search, dinfo(I).name); % just the name
+                  thisdata3 = load(thisfilename); %load just this file
+                  fprintf( 'File #%d, "%s", maximum value was: %g\n', K, ...
+                      thisfilename, max(thisdata3(:)) );
+                  if (max(thisdata3(:)) > maxIntensity)
+                      maxIntensity = max(thisdata3(:));
+                  end
+              end
+          end
       end
-  end
   end
 
 end
 
 % YHY peaks
 A0 = [1013 1013]; % x vector
-B0 = [0 15000];   % y vector
+B0 = [0 maxIntensity];   % y vector
 A1 = [1078 1078]; % x vector
-B1 = [0 15000];   % y vector
+B1 = [0 maxIntensity];   % y vector
 A2 = [1143 1143]; % x vector, pH sensitive
-B2 = [0 15000];   % y vector
+B2 = [0 maxIntensity];   % y vector
 A3 = [1182, 1182];% x vector
-B3 = [0 15000];   % y vector
+B3 = [0 maxIntensity];   % y vector
 A4 = [1430 1430]; % x vector, pH sensitive
-B4 = [0 15000];   % y vector
+B4 = [0 maxIntensity];   % y vector
 A5 = [1481 1481]; % x vector
-B5 = [0 15000];   % y vector
+B5 = [0 maxIntensity];   % y vector
 A6 = [1587 1587]; % x vector
-B6 = [0 15000];   % y vector
+B6 = [0 maxIntensity];   % y vector
 A7 = [1702 1702]; % x vector, pH sensitive
-B7 = [0 15000];   % y vector
-plot(thisdata1(:,1), thisdata1(:,2), 'blue', thisdata2(:,1), ...
-    thisdata2(:,2), 'cyan', thisdata3(:,1), thisdata3(:,2), 'magenta', ...
+B7 = [0 maxIntensity];   % y vector
+plot(thisdata1(:,1), thisdata1(:,2), 'black', thisdata2(:,1), ...
+    thisdata2(:,2), 'cyan', thisdata3(:,1), thisdata3(:,2), 'red', ...
     A0, B0, 'blue', A1, B1, 'blue', A2, B2, 'green', ...
     A3, B3, 'blue', A4, B4, 'green', A5, B5, 'blue', A6, B6, 'blue', ...
     A7, B7, 'green');
