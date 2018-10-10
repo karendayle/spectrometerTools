f3ff28378fe1e78977b6e21e7a62a64f4efaada6
kadeyo <karendayle@gmail.com>
2018-10-10 18:55:03 -0500

    Fixing colors and plot symbols
    Add global vars, use red for pH4, green for pH7 and blue for pH8.5

--- plotOneIntensityVsTime.m
+++ plotOneIntensityVsTime.m
@@ -2,18 +2,20 @@
 % intensity vs time by extracting it from a set of files in different directories
 % The time to use for the x axis is in the filename as
 % avg-yyyy-mm-dd-mm-ss.txt. Convert this to seconds since epoch.
+% Dayle Kotturi October 2018
 
 % There are two plots to build (or two lines on one plot).
 % Use the index 614 to get the intensity at 1430 cm^-1 (act. 1428.58 cm^-1)
+global x1;
 x1 = 614;
 % Use the index 794 to get the intensity at 1702 cm^-1 (act. 1701.95 cm^-1)
+global x2;
 x2 = 794;
 % Use the index 409 to get the intensity at the reference peak, 1078 cm^-1,
 % ring breathing
+global xRef;
 xRef = 409;
 % These indices could be modified slightly and results compared.
-
-% Dayle Kotturi October 2018
 
 % Colors:
 global blue;
@@ -24,6 +26,7 @@ global green;
 global ciel; 
 global cherry;
 global red;
+global black;
 blue =    [0.0000, 0.4470, 0.7410];
 rust =    [0.8500, 0.3250, 0.0980];
 gold =    [0.9290, 0.6940, 0.1250];
@@ -32,11 +35,17 @@ green =   [0.4660, 0.6740, 0.1880];
 ciel =    [0.3010, 0.7450, 0.9330];
 cherry =  [0.6350, 0.0780, 0.1840];
 red =     [1.0, 0.0, 0.0];
+black =   [0.0, 0.0, 0.0];
+pHcolor = [0.0, 0.0, 0.0];
+
+global numPoints;
+numPoints = 1024;
 
 % Change next 4 lines to what you want to plot
 % These are used to find the spectra that get plotted.
 % Multiple spectra in each subdir, but the latest one is used for plot
 % IMPORTANT: dirStem needs trailing backslash
+global dirStem;
 dirStem = "H:\Documents\Data\Embedded hydrogel study\flow through PNIPAM\";
 subDirStem1 = "pH4 1 cont 4 hrs 24 meas 10 min separ";
 subDirStem2 = "pH4 2 cont check signal 1 meas";
@@ -45,7 +54,7 @@ subDirStem4 = "pH7 1 cont 4 28 meas 10 min separ";
 subDirStem5 = "pH7 2 cont 2.4 hrs 15 meas 10 min separ";
 subDirStem6 = "pH8.5 1 cont 3.5 hrs 20 meas 10 min separ";
 subDirStem7 = "pH8.5 2 cont 2.25 hrs 14 meas 10 min separ";
-numPoints = 1024;
+
 thisData1 = zeros(2, numPoints, 'double');
 thisData2 = zeros(2, numPoints, 'double');
 thisData3 = zeros(2, numPoints, 'double');
@@ -54,16 +63,9 @@ thisData5 = zeros(2, numPoints, 'double');
 thisData6 = zeros(2, numPoints, 'double');
 thisData7 = zeros(2, numPoints, 'double');
 
-offset = 300;
-
 % Read in a set of spectra from a time-series 
 % Read in the name of the FOLDER.
 figure 
-xMin = 900;
-xMax = 2000;
-
-% initialize color
-lineColor = red;
 
 % subtract this offset 
 % myRefDate = '2018-10-01'
@@ -81,6 +83,7 @@ lineColor = red;
 %days apart: CHECK date 2018-09-01 10:00:00 has time 737304.4167
 %            CHECK date 2018-10-01 10:00:00 has time 737334.4167
 % So work with this approach for tRef and also further below
+global tRef;
 tRef = datenum(2018, 10, 01, 0, 0, 0);
 
 for K = 1:7
@@ -90,43 +93,51 @@ for K = 1:7
 
     switch K
         case 1
-            g = myPlot(dirStem, subDirStem1, thisData1, tRef, numPoints, x1, x2, xRef);
+            pHcolor = red;
+            g = myPlot(subDirStem1, thisData1, pHcolor);
         case 2
-            g = myPlot(dirStem, subDirStem2, thisData2, tRef, numPoints, x1, x2, xRef);
+            pHcolor = red;
+            g = myPlot(subDirStem2, thisData2, pHcolor);
         case 3
-            g = myPlot(dirStem, subDirStem3, thisData3, tRef, numPoints, x1, x2, xRef);               
+            pHcolor = red;
+            g = myPlot(subDirStem3, thisData3, pHcolor);               
         case 4
-            g = myPlot(dirStem, subDirStem4, thisData4, tRef, numPoints, x1, x2, xRef);
+            pHcolor = green;
+            g = myPlot(subDirStem4, thisData4, pHcolor);
         case 5
-            g = myPlot(dirStem, subDirStem5, thisData5, tRef, numPoints, x1, x2, xRef);
+            pHcolor = green;
+            g = myPlot(subDirStem5, thisData5, pHcolor);
         case 6
-            g = myPlot(dirStem, subDirStem6, thisData6, tRef, numPoints, x1, x2, xRef);
+            pHcolor = blue;
+            g = myPlot(subDirStem6, thisData6, pHcolor);
         case 7
-            g = myPlot(dirStem, subDirStem7, thisData7, tRef, numPoints, x1, x2, xRef);
+            pHcolor = blue;
+            g = myPlot(subDirStem7, thisData7, pHcolor);
     end
 end    
-% y = 1.3;
-% text(1710, y, 'Laser Power = 0.375 Max');
-% y = y - 0.1;
-% text(1710, y, '5 second integration time per acq');
-% y = y - 0.1;
-% text(1710, y, 'Each spectra average of 5 acqs');
-% y = y - 0.1;
-% text(1750, y, 'pH 4');
-% text(1710, y, '_____', 'Color', red);
-% y = y - 0.1;
-% text(1750, y, 'pH 7');
-% text(1710, y, '_____', 'Color', green);
-% y = y - 0.1;
-% text(1750, y, 'pH 8.5');
-% text(1710, y, '_____', 'Color', blue);
+y = 1.025;
+x = 5.;
+text(x+0.05, y, 'pH 4');
+text(x, y, '_____', 'Color', red);
+text(x+0.5, y, 'o', 'Color', black);
+text(x+0.6, y, '1430 cm^-1');
+
+y = y - 0.025;
+text(x+0.05, y, 'pH 7');
+text(x, y, '_____', 'Color', green);
+text(x+0.5, y, '*', 'Color', black);
+text(x+0.6, y, '1702 cm^-1');
+
+y = y - 0.025;
+text(x+0.05, y, 'pH 8.5');
+text(x, y, '_____', 'Color', blue);
 
 hold off
 title('Normalized intensity at pH-sensitive peaks vs time');
 xlabel('Time in days from epoch'); % x-axis label
 ylabel('Arbitrary Units (A.U.)/Intensity of ring-breathing at 1078 cm^-1'); % y-axis label
 
-function g = myPlot(dirStem, subDirStem, thisData, tRef, numPoints, xRef, x1, x2)
+function g = myPlot(subDirStem, thisData, myColor)
     global blue;
     global rust;
     global gold;
@@ -135,6 +146,12 @@ function g = myPlot(dirStem, subDirStem, thisData, tRef, numPoints, xRef, x1, x2
     global ciel; 
     global cherry;
     global red;
+    global dirStem;
+    global numPoints;
+    global x1;
+    global x2;
+    global xRef;
+    global tRef;
         
     str_dir_to_search = dirStem + subDirStem; % args need to be strings
     % This function only in 2018b:
@@ -187,9 +204,9 @@ function g = myPlot(dirStem, subDirStem, thisData, tRef, numPoints, xRef, x1, x2
         fclose(fileID);
     end
     % Now have points for the 1430 plot at t,y1 and for the 1702 plot at t,y2
-    plot(t,y1,'-o', 'Color', red);
+    plot(t,y1,'-o', 'Color', myColor);
     hold on;
-    plot(t,y2,'-o', 'Color', blue);
+    plot(t,y2,'-*', 'Color', myColor);
     hold on;
     g = 1;
 end
