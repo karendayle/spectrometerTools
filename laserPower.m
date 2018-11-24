f7e9befcdb8e15a8c58b1f37ef09931d5d4e1dc3
Dayle Kotturi <karendayle@gmail.com>
2018-11-23 22:32:09 -0600

    check in for backup

--- laserPower.m
+++ laserPower.m
@@ -9,7 +9,7 @@ xlabel('Fraction of full power')
 
 %bsc fitting
 %Result of bsc fitting
-fraction = [0.375 0.363 0.35 0.338 0.325]
+fraction = [0.005 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.375 0.363 0.35 0.338 0.325]
 power = 6.1e2*fraction - 1.8e2
 
 % what fraction is 20 mW?
