ff6f60dda60e95f6331162214d6c23f679c045e7
kadeyo <karendayle@gmail.com>
2018-12-10 22:37:55 -0600

    Update WasatchRealtime.m
    adding the dir creation

--- WasatchRealtime.m
+++ WasatchRealtime.m
@@ -91,9 +91,39 @@ fprintf('Found %s %s with %d pixels (%.2f, %.2fnm)\n', ...
 
 %%% NEW: Prompt for study name, append it to path, check it doesn't
 %%% exist, then create it.
-
-%%% NEW: Prompt for first pH buffer name (or other name for subdir)
-%%% create this subdir
+pwd
+prompt = '\nEnter name of dir to create> ';
+dir2 = input(prompt, 's');
+ 
+dir1 = '..\..\Data\Made by Sureyya\';
+s = strcat(dir1,dir2)
+s = strcat(s, '\')
+if ~exist(s, 'dir')
+    mkdir s
+    
+    % Make the subdirs
+    s1 = strcat(s, '1 pH7')
+    s2 = strcat(s, '2 pH4')
+    s3 = strcat(s, '3 pH10')
+    s4 = strcat(s, '4 pH7')
+    s5 = strcat(s, '5 pH10')
+    s6 = strcat(s, '6 pH4')
+    s7 = strcat(s, '7 pH10')
+    s8 = strcat(s, '8 pH7')
+    s9 = strcat(s, '9 pH4')
+    
+    mkdir(s1)
+    mkdir(s2)
+    mkdir(s3)
+    mkdir(s4)
+    mkdir(s5)
+    mkdir(s6)
+    mkdir(s7)
+    mkdir(s8)
+    mkdir(s9)
+else
+    fprintf('directory: %s exists', s);
+end
 
 % next: make this optional so you can see the peak unaltered and THEN enter
 % the index
