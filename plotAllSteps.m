336ac931394c1af31654da2561de9a37f7dd3abf
Dayle Kotturi <karendayle@gmail.com>
2020-10-01 15:45:14 -0500

    add two more samples
    add two more samples

--- plotAllSteps.m
+++ plotAllSteps.m
@@ -1,24 +1,32 @@
 numPoints = 1024;
 
-dirStem = "R:\Students\Dayle\Data\Made by Sureyya\Alginate\gel 0\MBA AuNPs 2\";
-
+% dirStem = "R:\Students\Dayle\Data\Made by Sureyya\Alginate\gel 0\MBA AuNPs 2\"; sample 1
+% dirStem = "R:\Students\Dayle\Data\Made by Sureyya\Alginate\gel 0\MBA AuNPs 19\"; % sample 2
+dirStem = "R:\Students\Dayle\Data\Made by Sureyya\Alginate\gel 0\MBA AuNPs 20\"; % sample 3
 % load(dirStem + 'matlabDark.mat');
 load(dirStem + 'matlabE.mat');
 load(dirStem + 'matlabF.mat');
 load(dirStem + 'matlabNormalized.mat');
 % load(dirStem + 'matlabRaw.mat');
 
-thisfilename = dirStem + "1\dark-2020-09-22-22-23-24.txt";
+% thisfilename = dirStem + "1\dark-2020-09-22-22-23-24.txt"; sample 1
+% thisfilename = dirStem + "1\dark-2020-09-30-20-56-11.txt"; % sample 2
+thisfilename = dirStem + "1\dark-2020-09-30-21-06-44.txt"; % sample 3
 fileID = fopen(thisfilename,'r');
 [dark] = fscanf(fileID, '%g %g', [2 numPoints]);
 fclose(fileID);
 
-thisfilename = dirStem + "1\rawSpectrum-2020-09-22-22-23-30.txt";
+% thisfilename = dirStem + "1\rawSpectrum-2020-09-22-22-23-30.txt"; sample 1
+% thisfilename = dirStem + "1\rawSpectrum-2020-09-30-20-56-29.txt"; % sample 2
+thisfilename = dirStem + "1\rawSpectrum-2020-09-30-21-06-50.txt"; % sample
+%3
 fileID = fopen(thisfilename,'r');
 [raw] = fscanf(fileID, '%g %g', [2 numPoints]);
 fclose(fileID);
 
-thisfilename = dirStem + "1\spectrum-2020-09-22-22-23-32.txt";
+% thisfilename = dirStem + "1\spectrum-2020-09-22-22-23-32.txt"; sample 1
+% thisfilename = dirStem + "1\spectrum-2020-09-30-20-56-31.txt"; % sample 2
+thisfilename = dirStem + "1\spectrum-2020-09-30-21-06-52.txt"; % sample 3
 fileID = fopen(thisfilename,'r');
 [spec] = fscanf(fileID, '%g %g', [2 numPoints]);
 fclose(fileID);
