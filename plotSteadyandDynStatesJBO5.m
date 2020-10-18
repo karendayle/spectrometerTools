142711ae6d61b277bcf176dbaccc3da2ada35f99
Dayle Kotturi <karendayle@gmail.com>
2020-10-18 18:29:57 -0500

    Almost done with hatched fill
    Now to just hatch only RHS of each pair

--- plotSteadyandDynStatesJBO5.m
+++ plotSteadyandDynStatesJBO5.m
@@ -515,6 +515,11 @@ function h = plotBarOfAvgsSideBySide(yBar, yErr)
     % There are 4 groups (gels) of 2 bars each
     % Fill them with white and hatch fill the static values in later
     a = bar(yBar, 'FaceColor','w'); % This works. See 4 groups of 2 bars each
+    
+    % Yes, a bit silly to pass in both, but I need 'a' and it's not working
+    % to extract yBar back out and doc'n not helping, so doing it this way.
+    fillBarsWithHatchedLines(a, yBar);
+    
     % But don't have the x data, so how to plot the errorbars?
     xd = get(a,'xdata');
     hold on;
@@ -556,5 +561,42 @@ function [avgA, stdDevA] = buildArrayForBars(gel, pHlevel)
         avgA = mean(A); 
         stdDevA = std(A);
     end
+end
 
+function k = fillBarsWithHatchedLines(B, yd)
+    bw = 0.23; % deduce by trial and error plotting over actual drawn bar
+
+    % helpful: https://www.mathworks.com/matlabcentral/answers/203782-how-to-determine-the-x-axis-positions-of-the-bars-in-a-grouped-bar-chart
+    for ib = 1:numel(B)
+        % important: xData is has the center pts of each bar of group 1 in
+        % row 1 and the center pt of each bar of group 2 in row 2
+        xData(:,ib) = B(ib).XData+B(ib).XOffset;
+    end
+
+    % Now generalize it in a loop
+    [rows, cols] = size(yd);
+    for i = 1:rows
+        for j = 1:cols
+            fprintf('row %d, col %d', i, j);
+            % draw the slanted lines that go all the way across
+            y1 = 0; 
+            ymax = yd(i,j); 
+
+            % this is the only "weakpoint". The dy depends on ymax value,
+            % so the hatchline spacing will vary across bars. This could be
+            % changed by using same dy for all bars, but the range of k needs
+            % to change as well
+            dy = (ymax - y1)/10;
+            for k = 1:10
+                x1 = xData(i,j) - bw/2;
+                x2 = xData(i,j) + bw/2;   
+                % don't draw a line that exceeds ymax
+                y2 = min(y1 + dy, ymax);
+                fprintf ('%d-%d.%d bw=%f x1=%f x2=%f y1=%f y2=%f\n', i, j, k, bw, x1, x2, y1, y2);
+                line([x1 x2], [y1 y2],'linewidth',1,'color','k');
+                y1 = y1 + dy;
+            end
+        end
+    end
+    k = 1;
 end
