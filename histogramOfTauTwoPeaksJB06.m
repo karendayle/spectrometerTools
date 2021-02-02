851c17e69bf3859c3750e28fc2e5946545467fae
Dayle Kotturi <karendayle@gmail.com>
2021-02-01 23:47:07 -0600

    looking good

--- histogramOfTauTwoPeaksJB06.m
+++ histogramOfTauTwoPeaksJB06.m
@@ -1,5 +1,18 @@
 % Dayle Kotturi January 2021
 % input=loads vals saved by plotRatiosOfTimeSeriesJB03
+% RGB
+blue =    [0.0000, 0.4470, 0.7410];
+rust =    [0.8500, 0.3250, 0.0980];
+gold =    [0.9290, 0.6940, 0.1250];
+purple =  [0.4940, 0.1840, 0.5560];
+green =   [0.4660, 0.6740, 0.1880];
+ciel =    [0.3010, 0.7450, 0.9330];
+cherry =  [0.6350, 0.0780, 0.1840];
+red =     [1.0, 0.0, 0.0];
+black =   [0.0, 0.0, 0.0];
+magenta = [1.0, 0.0, 1.0];
+teal =    [0.2, 0.6, 0.5];
+
 %% kdk: Clear previous plots
 close all
 
@@ -9,20 +22,14 @@ load ('Data\vals.mat');
 global tallyByGel
 
 global autoSave
-autosave = 0; % CHOOSE 1 to save plots to files, 0 to do this manually
+autoSave = 1; % CHOOSE 1 to save plots to files, 0 to do this manually
 
-% 0-10m, 10m-1h, 1h-1d, >1d
-% First row: the 1430 peaks
-% Second row:  the 1702 peaks
+% Bins are: 0-10m, 10m-1h, 1h-1d, >1d
 tallyByGel = [ ...
-         0, 0, 0, 0, ... % all gels (alg, PEG, pHEMA, pHE-coA) for bin #1
-         0, 0, 0, 0, ... % all gels for bin #2
-         0, 0, 0, 0, ... % all gels for bin #3
-         0, 0, 0, 0; ...  % all gels for bin #4
-         0, 0, 0, 0, ... % all gels for bin #1
-         0, 0, 0, 0, ... % all gels for bin #2
-         0, 0, 0, 0, ... % all gels for bin #3
-         0, 0, 0, 0];    % all gels for bin #4
+    0, 0, 0, 0, 0, 0, 0, 0; % bin1, all gels, 2 pks each
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0 ];
 
 for gel = 1:4 % one gel at a time
     for series = 1:3 % all series for each gel
@@ -42,28 +49,32 @@ else
     figure
 end
 
-for ii = 1:2
-    myArray = tallyByGel(ii,:);
-    myArray = reshape(myArray,4,4);
-    % idea is to manually set the x coords for each peak
-    % so that they appear interleaved. set breakpoint at hold on
-    % to see the orer it gets drawn
-    switch ii
-        case 1
-            myX = [1, 2, 3, 4; 6, 7, 8, 9; 11, 12, 13, 14; 16, 17, 18, 19];
-        case 2
-            myX = [1.5, 2.5, 3.5, 4.5; 6.5, 7.5, 8.5, 9.5; 11.5, 12.5, ...
-                13.5, 14.5; 16.5, 17.5, 18.5, 19.5];
-    end
-                
-    a = bar(myX, myArray, 'FaceColor','w');
-    %fillBarsWithHatchedLines(a, myArray, ii);
-    hold on;
-end
+myArray = reshape(tallyByGel,4,8);
+% This is the trick to get the groupings correct
+myX = [1, 1, 1, 1, 1, 1, 1, 1; ...
+    2, 2, 2, 2, 2, 2, 2, 2; ...
+    3, 3, 3, 3, 3, 3, 3, 3; ...
+    4, 4, 4, 4, 4, 4, 4, 4];
+
+%OLD a = bar(myX, myArray, 'FaceColor','w');
+% THIS COLORS EACH BAR DIFFERENTLY a = bar(myX, myArray);
+a = bar(myX, myArray);
+% color by gel
+a(1).FaceColor = gold; % alginate
+a(2).FaceColor = gold;
+a(3).FaceColor = teal; % PEG
+a(4).FaceColor = teal;
+a(5).FaceColor = rust; % pHEMA
+a(6).FaceColor = rust;
+a(7).FaceColor = blue; % pHEMA/coA
+a(8).FaceColor = blue;
+% want all the 1702 peaks to have the hatched lines
+fillBarsWithHatchedLines(a, myArray); 
+
 myLabelFont = 30;
-xlabel('Value of Tau (hr)', 'FontSize', myLabelFont); % x-axis label
+xlabel('Time constant, tau', 'FontSize', myLabelFont); % x-axis label
 ylabel('Number of time constants', 'FontSize', myLabelFont); % y-axis label
-fname = {'<10m';'<1h';'<1d';'>1d'}; % FIX! 
+fname = {'<10 min';'<1 hour';'<1 day';'>1 day'}; % FIX! 
 set(gca, 'XTick', 1:length(fname),'XTickLabel',fname);
 set(gca, 'FontSize', 30,'FontWeight','bold','box','off');
 myTitle = sprintf('taus for both peaks');
@@ -72,6 +83,7 @@ if autoSave
 end
 %end main portion
 
+% determine bin# from tau
 function a = addToTallyByGel(tau, gel, peak)
     global tallyByGel
     
@@ -88,28 +100,32 @@ function a = addToTallyByGel(tau, gel, peak)
             end
         end
     end 
-    offset = gel + (bin - 1) * 4; 
-    tallyByGel(peak, offset) = tallyByGel(peak, offset) + 1;
-    fprintf('peak%d bin%d gel%d tau=%f gets added to tally(%d,%d)\n', ...
-    peak, bin, gel, tau, peak, offset);
+    % OLD: offset = gel + (bin - 1) * 4; 
+    % OLD: tallyByGel(peak, offset) = tallyByGel(peak, offset) + 1;
+    % 2021/02/01
+    offset = (gel - 1) * 2 + peak; 
+    tallyByGel(bin, offset) = tallyByGel(bin, offset) + 1;
+    fprintf('bin%d gel%d peak%d tau=%f gets added to tally(%d,%d)\n', ...
+    bin, gel, peak, tau, bin, offset);
     a = 1;
 end
 
-function k = fillBarsWithHatchedLines(B, yd, iter)
-    bw = 0.15; % deduce by trial and error plotting over actual drawn bar
-
+function k = fillBarsWithHatchedLines(B, yd)
+    bw = 0.08; % deduce by trial and error plotting over actual drawn bar
+    
     % helpful: https://www.mathworks.com/matlabcentral/answers/203782-how-to-determine-the-x-axis-positions-of-the-bars-in-a-grouped-bar-chart
     for ib = 1:numel(B)
         % important: xData is has the center pts of each bar of group 1 in
         % row 1 and the center pt of each bar of group 2 in row 2
         xData(:,ib) = B(ib).XData+B(ib).XOffset;
+        [rows, cols] = size(xData);
+        fprintf('rows %d cols %d\n', rows, cols);
     end
 
     % Now generalize it in a loop
     [rows, cols] = size(yd);
     for i = 1:rows
-        
-        for j = 1:2:cols
+        for j = 2:2:cols
             % draw the slanted lines that go all the way across
             y1 = 0; 
             y2 = 0;
@@ -122,17 +138,11 @@ function k = fillBarsWithHatchedLines(B, yd, iter)
             %dy = (ymax - y1)/10;
             %fprintf('dy is %f', dy);
             %dy = 0.004; % standardize on this for all bars
-            dy = 0.05 * max(yd(:,j)); % standardize on this for all bars based on max
+            dy = 0.025 * max(yd(:,j)); % standardize on this for all bars based on max
             maxK = ceil(ymax/dy); 
             for k = 1:maxK
-                switch iter
-                    case 1
-                        x1 = xData(i,j) - bw/4;
-                        x2 = xData(i,j) + bw/4;
-                    case 2
-                        x1 = xData(i,j) - bw/4 + bw/2;
-                        x2 = xData(i,j) + bw/4 + bw/2;
-                end
+                x1 = xData(i,j) - bw/2;
+                x2 = xData(i,j) + bw/2;
                 y2 = y2 + dy;
                 % don't draw a line that exceeds ymax
                 % use eqn of a line to find a new x2
