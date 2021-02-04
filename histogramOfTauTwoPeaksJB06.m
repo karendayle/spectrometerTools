1f27608118abf64199ef3f90a17fec68afcfd483
Dayle Kotturi <karendayle@gmail.com>
2021-02-04 17:37:11 -0600

    add plots showing tau by pH

--- histogramOfTauTwoPeaksJB06.m
+++ histogramOfTauTwoPeaksJB06.m
@@ -20,6 +20,7 @@ close all
 load ('Data\vals.mat');
 
 global tallyByGel
+global tallyByPH
 
 global autoSave
 autoSave = 1; % CHOOSE 1 to save plots to files, 0 to do this manually
@@ -31,20 +32,40 @@ tallyByGel = [ ...
     0, 0, 0, 0, 0, 0, 0, 0;
     0, 0, 0, 0, 0, 0, 0, 0 ];
 
+% Bins are: 0-10m, 10m-1h, 1h-1d, >1d
+% pH4
+tallyByPH(:,:,1) = [ ...
+    0, 0, 0, 0, 0, 0, 0, 0; % bin1, all gels, 2 pks each
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0 ];
+% pH7
+tallyByPH(:,:,2) = [ ...
+    0, 0, 0, 0, 0, 0, 0, 0; % bin1, all gels, 2 pks each
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0 ];
+% pH10
+tallyByPH(:,:,3) = [ ...
+    0, 0, 0, 0, 0, 0, 0, 0; % bin1, all gels, 2 pks each
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0;
+    0, 0, 0, 0, 0, 0, 0, 0 ];
+
 for gel = 1:4 % one gel at a time
     for series = 1:3 % all series for each gel
         for segment = 1:9 % all pH segments in each series
             tau = 1.0/(vals(gel, series, segment, 1, 4)); % 1430 peak
-            addToTallyByGel(tau, gel, 1);
+            % addToTallyByGel(tau, gel, 1);
             addToTallyByPH(tau, gel, getPHValue(segment), 1);
             tau = 1.0/(vals(gel, series, segment, 2, 4)); % 1702 peak
-            addToTallyByGel(tau, gel, 2);
+            % addToTallyByGel(tau, gel, 2);
             addToTallyByPH(tau, gel, getPHValue(segment), 2);
         end
     end
 end
 
-% set up one plot
+% set up first plot
 if autoSave
     FigH = figure('Position', get(0, 'Screensize'));
 else
@@ -83,6 +104,52 @@ myTitle = sprintf('taus for both peaks');
 if autoSave
     saveMyPlot(FigH, myTitle);
 end
+
+% NEW 2021/02/04 show dist'n of tau over pH levels 4, 7, 10
+% set up rest of the plots
+
+for ii = 1:3
+    if autoSave
+        FigH = figure('Position', get(0, 'Screensize'));
+    else
+        figure
+    end
+
+    tallyBySinglePH = tallyByPH(:,:,ii);
+
+    myArray = reshape(tallyBySinglePH,4,8);
+    % This is the trick to get the groupings correct
+    myX = [1, 1, 1, 1, 1, 1, 1, 1; ...
+        2, 2, 2, 2, 2, 2, 2, 2; ...
+        3, 3, 3, 3, 3, 3, 3, 3; ...
+        4, 4, 4, 4, 4, 4, 4, 4];
+
+    %OLD a = bar(myX, myArray, 'FaceColor','w');
+    % THIS COLORS EACH BAR DIFFERENTLY a = bar(myX, myArray);
+    a = bar(myX, myArray);
+    % color by gel
+    a(1).FaceColor = gold; % alginate
+    a(2).FaceColor = gold;
+    a(3).FaceColor = teal; % PEG
+    a(4).FaceColor = teal;
+    a(5).FaceColor = rust; % pHEMA
+    a(6).FaceColor = rust;
+    a(7).FaceColor = blue; % pHEMA/coA
+    a(8).FaceColor = blue;
+    % want all the 1702 peaks to have the hatched lines
+    fillBarsWithHatchedLines(a, myArray); 
+
+    myLabelFont = 30;
+    xlabel('Time constant, tau', 'FontSize', myLabelFont); % x-axis label
+    ylabel('Number of time constants', 'FontSize', myLabelFont); % y-axis label
+    fname = {'<10 min';'<1 hour';'<1 day';'>1 day'}; % FIX! 
+    set(gca, 'XTick', 1:length(fname),'XTickLabel',fname);
+    set(gca, 'FontSize', 30,'FontWeight','bold','box','off');
+    myTitle = sprintf('taus for both peaks');
+    if autoSave
+        saveMyPlot(FigH, myTitle);
+    end
+end
 %end main portion
 
 % determine bin# from tau
@@ -102,12 +169,9 @@ function a = addToTallyByGel(tau, gel, peak)
             end
         end
     end 
-    % OLD: offset = gel + (bin - 1) * 4; 
-    % OLD: tallyByGel(peak, offset) = tallyByGel(peak, offset) + 1;
-    % 2021/02/01
     offset = (gel - 1) * 2 + peak; 
     tallyByGel(bin, offset) = tallyByGel(bin, offset) + 1;
-    fprintf('bin%d gel%d peak%d tau=%f gets added to tally(%d,%d)\n', ...
+    fprintf('addToTallyByGel: bin%d gel%d peak%d tau=%f gets added to tally(%d,%d)\n', ...
     bin, gel, peak, tau, bin, offset);
     a = 1;
 end
@@ -125,7 +189,42 @@ function b = getPHValue(segment)
 end
 
 function c = addToTallyByPH(tau, gel, pHValue, peak)
-% START HERE
+    global tallyByPH
+    global tallyByGel
+    
+    if tau < (1.0/6.0)
+        bin = 1;
+    else 
+        if tau < 1.0
+            bin = 2;
+        else
+            if tau < 24.0
+                bin = 3;
+            else
+                bin = 4;
+            end
+        end
+    end 
+
+    offset = (gel - 1) * 2 + peak; 
+    tallyByGel(bin, offset) = tallyByGel(bin, offset) + 1;
+    fprintf('addToTallyByGel: bin%d gel%d peak%d tau=%f gets added to tallyByGel(%d,%d)\n', ...
+    bin, gel, peak, tau, bin, offset);
+
+    % Additional for pHIndexing
+    switch pHValue
+        case 4
+            pHIndex = 1;
+        case 7
+            pHIndex = 2;
+        case 10
+            pHIndex = 3;     
+    end
+    tallyByPH(bin, offset, pHIndex) = ...
+        tallyByPH(bin, offset, pHIndex) + 1;
+    
+    fprintf('addToTallyByPH: bin%d gel%d peak%d pH%d tau=%f gets added to tallyByPH(%d,%d)\n', ...
+    bin, gel, peak, pHValue, tau, bin, offset);
     c = 1;
 end
 
@@ -138,7 +237,7 @@ function k = fillBarsWithHatchedLines(B, yd)
         % row 1 and the center pt of each bar of group 2 in row 2
         xData(:,ib) = B(ib).XData+B(ib).XOffset;
         [rows, cols] = size(xData);
-        fprintf('rows %d cols %d\n', rows, cols);
+        % fprintf('rows %d cols %d\n', rows, cols);
     end
 
     % Now generalize it in a loop
@@ -149,7 +248,7 @@ function k = fillBarsWithHatchedLines(B, yd)
             y1 = 0; 
             y2 = 0;
             ymax = yd(i,j); 
-            fprintf('row %d, col %d: %f', i, j, ymax);
+            % fprintf('row %d, col %d: %f', i, j, ymax);
             % this is the only "weakpoint". The dy depends on ymax value,
             % so the hatchline spacing will vary across bars. This could be
             % changed by using same dy for all bars, but the range of k needs
@@ -171,7 +270,7 @@ function k = fillBarsWithHatchedLines(B, yd)
                     y2 = ymax;
                     x2 = (y2 - b)/m;
                 end
-                fprintf ('%d-%d.%d bw=%f x1=%f x2=%f y1=%f y2=%f\n', i, j, k, bw, x1, x2, y1, y2);
+                % fprintf ('%d-%d.%d bw=%f x1=%f x2=%f y1=%f y2=%f\n', i, j, k, bw, x1, x2, y1, y2);
                 line([x1 x2], [y1 y2],'linewidth',1,'color','k');
                 y1 = y1 + dy;
             end
