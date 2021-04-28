0c36949f4586b1b08711616a8e3d9b0361a23b62
Dayle Kotturi <karendayle@gmail.com>
2021-04-27 19:35:44 -0500

    Update checkDatasetAfterAcquisition.m
    changes before adding loop for all datasets

--- dataProcessing/checkDatasetAfterAcquisition.m
+++ dataProcessing/checkDatasetAfterAcquisition.m
@@ -2,14 +2,18 @@
 % Use to check a dataset
 
 % Dayle Kotturi April 2021
-
+close all;
+xMin = 950;
+xMax = 1800;
 numPoints = 1024;
 % Read in a set of spectra from a time-series 
 % Read in the name of the FOLDER.
 % CHOOSE (specify) the top level directory of the dataset
 % dir_to_search = 'C:\Users\dayle.kotturi\Documents\Data\SORS1';
 %dir_to_search = 'R:\Students\Dayle\Data\Made by Waqas\Blank gels\gel 4\blank3\1';
-dir_to_search = 'R:\BMEN\McShane_Mike\Students\Dayle\Data\Made by Waqas\other\gel 4\redo blank3\1';
+dir_to_search = 'R:\Students\Dayle\Data\Made by Waqas\Blank gels\gel 4\redo blank1\1';
+% dir_to_search = 'R:\Students\Dayle\Data\Made by Waqas\Blank gels\gel 4\redo blank2\1';
+% dir_to_search = 'R:\Students\Dayle\Data\Made by Waqas\Blank gels\gel 4\redo blank3\1';
 
 % 1. PLOT THE DARK
 txtpattern = fullfile(dir_to_search, 'dark*.txt');
@@ -28,6 +32,10 @@ for K = 1 : length(dinfo)
 end
 myTitle = sprintf('dark spectrum for %s', dir_to_search);
 title(myTitle);
+xlim([xMin xMax]);
+xlabel('Wavenumber (cm^-^1)'); % x-axis label
+ylabel('Intensity (Arbitrary Units)'); % y-axis label
+set(gca,'FontSize',15,'FontWeight','bold','box','off'); % used for title and label
 
 % 2. PLOT THE RAW SPECTRA, 2 DIFFERENT WAYS
 txtpattern = fullfile(dir_to_search, 'raw*.txt');
@@ -45,7 +53,10 @@ for K = 1 : length(dinfo)
   pause(1);
 end
 myTitle = sprintf('All raw spectra for %s', dir_to_search);
+xlim([xMin xMax]);
 title(myTitle);
+xlabel('Wavenumber (cm^-^1)'); % x-axis label
+ylabel('Intensity (Arbitrary Units)'); % y-axis label
 
 txtpattern = fullfile(dir_to_search, 'raw*.txt');
 dinfo = dir(txtpattern);
@@ -66,6 +77,9 @@ for K = 1 : length(dinfo)
 end
 myTitle = sprintf('3D of all raw spectra for %s', dir_to_search);
 title(myTitle);
+xlabel('Wavenumber (cm^-^1)'); % x-axis label
+ylabel('Intensity (Arbitrary Units)'); % y-axis label
+set(gca,'FontSize',15,'FontWeight','bold','box','off'); % used for title and label
 
 % 2. PLOT THE AVG SPECTRA, 2 DIFFERENT WAYS
 txtpattern = fullfile(dir_to_search, 'avg*.txt');
@@ -83,7 +97,11 @@ for K = 1 : length(dinfo)
   pause(1);
 end
 myTitle = sprintf('All avg spectra for %s', dir_to_search);
+set(gca,'FontSize',15,'FontWeight','bold','box','off'); % used for title and label
 title(myTitle);
+xlim([xMin xMax]);
+xlabel('Wavenumber (cm^-^1)'); % x-axis label
+ylabel('Intensity (Arbitrary Units)'); % y-axis label
 
 txtpattern = fullfile(dir_to_search, 'avg*.txt');
 dinfo = dir(txtpattern);
@@ -103,7 +121,12 @@ for K = 1 : length(dinfo)
   pause(1);
 end
 myTitle = sprintf('3D of all avg spectra for %s', dir_to_search);
+set(gca,'FontSize',15,'FontWeight','bold','box','off'); % used for title and label
+xlim([xMin xMax]);
 title(myTitle);
+xlabel('Wavenumber (cm^-^1)'); % x-axis label
+ylabel('Measurement #'); % y-axis label
+zlabel('Intensity (Arbitrary Units)'); % z-axis label
 
 % Plot each spectrum (intensity vs wavenumber in a new color overtop
 
